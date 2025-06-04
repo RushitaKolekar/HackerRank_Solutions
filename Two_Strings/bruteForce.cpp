d56e25c60c9a673c473c142a387fe5892f35fced
Rushita Kolekar <kolekarrushita@gmail.com>
2025-06-04 21:56:29 +0530

    updated

--- Two_Strings/bruteForce.cpp
+++ Two_Strings/bruteForce.cpp
@@ -11,4 +11,4 @@ string twoStrings(string s1, string s2) {
 }
 
 // Time Complexity: O(n*m) where n is the length of s1 and m is the length of s2
-// Space Complexity: O(1) since we are not using any extra space that grows with input size
+// Space Complexity: O(1) 
